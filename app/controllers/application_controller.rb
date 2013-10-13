b1ad63f05cfa50b3e860774eec291b857850ddce
Philip Nunoo <ketay99@gmail.com>
2013-10-13 20:28:47 +0000

    removed changes in application.rb file

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,4 @@
 class ApplicationController < ActionController::
 	protect_from_forgery
-	before_filter :strip_www
-
-	def strip_www
-	  if request.env["HTTP_HOST"] == "www.aplanke.herokuapp.com"
-	    redirect_to "http://www.aplanke.phnunoo.com/"
-	  end
-	end
+	
 end
