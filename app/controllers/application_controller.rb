cd479da214c70a90b464161c921ae6099b8eea4f
Philip Nunoo <ketay99@gmail.com>
2013-10-13 20:24:02 +0000

    adding redirection to application.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
-class ApplicationController < ActionController::Base
-  protect_from_forgery
+class ApplicationController < ActionController::
+	protect_from_forgery
+	before_filter :strip_www
+
+	def strip_www
+	  if request.env["HTTP_HOST"] == "www.aplanke.herokuapp.com"
+	    redirect_to "http://www.aplanke.phnunoo.com/"
+	  end
+	end
 end
