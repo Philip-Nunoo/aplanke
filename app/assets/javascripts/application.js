c5196300ffe64d460186af95545bbe6866d0efb8
Philip Nunoo <ketay99@gmail.com>
2013-10-14 13:17:53 +0000

    worked on facebook social

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,12 @@
 //= require_tree .
 
 $(function(){ $(document).foundation(); });
+
+// facebook-jssdk
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_GB/all.js#xfbml=1&appId=633047826708725";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
